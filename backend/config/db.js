dd75a36289ce28b27eaa8473e68368927e0adb8b
Hridya Mani <hridyamani123@gmail.com>
2026-03-25 01:33:50 +0530

    commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -13,7 +13,7 @@ const connectDB = async () => {
     };
     
     const conn = await mongoose.connect(process.env.MONGODB_URI, options);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log("MongoDB Connected");
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
